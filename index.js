115b6deb12d65b61e149ef1649b21440f3927c1f
moyuh <mlroth94@gmail..com>
2022-08-21 00:21:00 -0600

    adds function to index and markdown

--- index.js
+++ index.js
@@ -1,13 +1,77 @@
+// https://img.shields.io/github/license/moyuh/readme-generator
+
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [{
+inquirer
+  .prompt([{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Please describe your project, include what it is for and why you made it:',
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'How do you install your project? Provide step-by-step instructions',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'What will your project be used for?',
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'Which license was used for your project?',
+    choices: ['apache','MIT','GNU','no license used'],
+},
+{
+    type: 'input',
+    name: 'contributer',
+    message: 'Who has contributed to help make this project possible?',
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'How can you test your application?',
+},
+{
+    type: 'input',
+    name: 'githubUsername',
+    message: 'What is your github username?',
+},
+{
+    type: 'input',
+    name: 'githubURL',
+    message: 'What is your github url?',
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+},
+])
+.then((answers) => {
+    generateMarkdown(answers)
+
+    fs.writeFile('README.md', generateMarkdown, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,48 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license !== 'no license used'){
+    return `![badge][https://img.shields.io/github/license/moyuh/readme-generator]${license}`;
+  } else{
+    return ' ';
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license !== 'no license used'){
+    return `[${license}](https://choosealicense.com/license${license})`
+  } else {
+    return ' ';
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license used'){
+    return `##[${license}](#table-of-contents) This project is under the following license: 
+    ${renderLicenseLink(license)}`;
+  } else{
+    return '';
+  }
+};
+
+function renderLicenseToTableOfContents(license) {
+  if (license !== 'no license used'){
+    return `*[License](#license)`;
+  } else{
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## Table-of-Contents
+
 
 `;
 }
